d5faddabe7f45fb204bd70daa3e9b445ca368140
farahcaa <144845898+farahcaa@users.noreply.github.com>
2025-11-08 20:54:51 -0500

    Feat fixed

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 
 const Layout = () => {
   return (
-    <div className="w-[100vw] min-h-[100vh] flex border flex-col">
+    <div className=" min-h-[100vh] flex border flex-col">
       <div className="sticky top-0 z-50 bg-white">
         <Header />
       </div>
